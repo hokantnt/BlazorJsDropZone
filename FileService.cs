cd3b40af54799654d7872c034647c6db9519d947
Jacob Östelid <Jacob.Ostelid@xylem.com>
2024-11-14 18:52:55 +0100

    Add simple example from web and mod to work in solution

---
+++ FileService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Collections.ObjectModel;
+
+
+namespace FilepickerLab;
+
+public class FileService
+{
+    public ObservableCollection<IBrowserFile> LoadedFiles { get; } = [];
+}
